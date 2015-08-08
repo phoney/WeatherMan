7807922dadd4c6b5ed9feeb341de9becdf31badb
Brian Stern <brians99@bellsouth.net>
2015-08-08 18:14:49 -0400

    Pass the zip code to the detail view controller

--- WeatherMan/DetailViewController.swift
+++ WeatherMan/DetailViewController.swift
@@ -12,9 +12,11 @@ class DetailViewController: UITableViewController {
 
 	var objects = [AnyObject]()
 
-	var detailItem: AnyObject? {
+	var detailItem: String? {
 		didSet {
 		    // Update the view.
+			println("zipcode \(detailItem)")
+			self.fetchWeather()
 		    self.configureView()
 		}
 	}
@@ -36,10 +38,6 @@ class DetailViewController: UITableViewController {
 
 	// MARK: - Table View
 	
-	override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-		return 1
-	}
-	
 	override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 		return objects.count
 	}
@@ -51,21 +49,13 @@ class DetailViewController: UITableViewController {
 		cell.textLabel!.text = object.description
 		return cell
 	}
-	
-	override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-		// Return false if you do not want the specified item to be editable.
-		return true
-	}
-	
-	override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-		if editingStyle == .Delete {
-			objects.removeAtIndex(indexPath.row)
-			tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-		} else if editingStyle == .Insert {
-			// Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
-		}
-	}
 
+	// MARK: - Weather
+	
+	func fetchWeather() {
+		
+	}
+	
 
 }
 
--- WeatherMan/MasterViewController.swift
+++ WeatherMan/MasterViewController.swift
@@ -66,16 +66,12 @@ class MasterViewController: UIViewController, UITextFieldDelegate {
 
 	override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
 		if segue.identifier == "showDetail" {
-
-			/*
-			if let indexPath = self.tableView.indexPathForSelectedRow() {
-		        let object = objects[indexPath.row] as! NSDate
+			if let zipcode = self.zipcodeTextField.text {
 		        let controller = (segue.destinationViewController as! UINavigationController).topViewController as! DetailViewController
-		        controller.detailItem = object
+		        controller.detailItem = zipcode
 		        controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
 		        controller.navigationItem.leftItemsSupplementBackButton = true
 		    }
-*/
 		}
 	}
 
